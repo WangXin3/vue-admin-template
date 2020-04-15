516ce977dd787b3189b54ae1828a1a69751d64b7
wangx <91907@163.com>
2020-04-15 22:40:05 +0800

    新增用户

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,7 +1,50 @@
 <template>
-  <div>
+  <div style="padding: 20px">
     <el-row>
-      <el-col :span="24">
+      <el-col>
+        <el-button type="primary" @click="dialogFormVisible = true">添加</el-button>
+        <el-button type="danger" :disabled="selectData.length === 0" @click="deleteUser(selectData)">删除</el-button>
+      </el-col>
+    </el-row>
+
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisible" width="20%" @close="close">
+      <el-form ref="user" :model="user" :rules="rules">
+        <el-form-item label="用户名" label-width="90px" size="medium" prop="username" required>
+          <el-input v-model="user.username" placeholder="请输入用户名" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="昵称" label-width="90px" size="medium" prop="nickname" required>
+          <el-input v-model="user.nickname" placeholder="请输入昵称" />
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="90px" size="medium" prop="email" required>
+          <el-input v-model="user.email" placeholder="请输入Email" />
+        </el-form-item>
+        <el-form-item label="角色" label-width="90px" size="medium" prop="roles" required>
+          <el-select v-model="user.roles" multiple placeholder="请选择">
+            <el-option
+              v-for="item in roles"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="性别" label-width="90px" size="medium" prop="gender">
+          <el-radio v-model="user.gender" label="1">男</el-radio>
+          <el-radio v-model="user.gender" label="2">女</el-radio>
+        </el-form-item>
+        <el-form-item label="状态" label-width="90px" size="medium" prop="enabled">
+          <el-radio v-model="user.enabled" label="1">激活</el-radio>
+          <el-radio v-model="user.enabled" label="2">禁用</el-radio>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser('user')">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <el-row>
+      <el-col>
         <el-table
           v-loading="listLoading"
           :data="tableData"
@@ -9,7 +52,12 @@
           stripe
           empty-text="暂无数据"
           style="width: 100%"
+          @selection-change="handleSelectionChange"
         >
+          <el-table-column
+            type="selection"
+            min-width="200"
+          />
           <el-table-column
             label="头像"
             align="center"
@@ -34,7 +82,8 @@
           <el-table-column
             label="性别"
             align="center"
-            min-width="120">
+            min-width="120"
+          >
             <template slot-scope="scope">
               <span>{{ scope.row.gender == 0 ? '女' : '男' }}</span>
             </template>
@@ -54,10 +103,11 @@
           <el-table-column
             label="状态"
             align="center"
-            min-width="150">
+            min-width="150"
+          >
             <template slot-scope="scope">
-              <el-tag type="success" v-if="scope.row.enabled">正常</el-tag>
-              <el-tag type="danger" v-else>禁用</el-tag>
+              <el-tag v-if="scope.row.enabled" type="success">激活</el-tag>
+              <el-tag v-else type="danger">禁用</el-tag>
             </template>
           </el-table-column>
           <el-table-column
@@ -67,8 +117,8 @@
             min-width="100"
           >
             <template slot-scope="scope">
-              <el-button type="text" size="small" @click="handleClick(scope.row)">查看</el-button>
-              <el-button type="text" size="small">编辑</el-button>
+              <el-button type="text" size="small" @click="handleClick(scope.row)">编辑</el-button>
+              <el-button type="text" size="small" @click="deleteUser(scope.row)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -92,29 +142,105 @@ export default {
   data() {
     return {
       tableData: [],
-      listLoading: true
+      listLoading: true,
+      selectData: [],
+      user: {
+        username: '',
+        nickname: '',
+        email: '',
+        roles: [],
+        gender: '1',
+        enabled: '1'
+      },
+      dialogFormVisible: false,
+
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        nickname: [
+          { required: true, message: '请输入昵称', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
+        ],
+        roles: [
+          { required: true, message: '请选择角色', trigger: 'change' }
+        ],
+        gender: [],
+        enabled: []
+      },
+      roles: [
+        { value: 'admin', label: '超级管理员' },
+        { value: 'user', label: '用户' }
+      ]
     }
   },
   created() {
     this.fetchData()
   },
   methods: {
+    // 编辑用户
     handleClick(row) {
       console.log(row)
     },
 
+    // 加载数据
     fetchData() {
       this.listLoading = true
       getList().then(response => {
         this.tableData = response.data
-        console.log(this.tableData)
         this.listLoading = false
       })
+    },
+
+    // 选择用户
+    handleSelectionChange(val) {
+      this.selectData = val
+    },
+
+    // 删除用户
+    deleteUser(users) {
+      this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        console.log(users)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+
+      })
+    },
+
+    // 新增用户
+    addUser(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+          this.dialogFormVisible = false
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+
+    // 关闭新增用户dialog
+    close() {
+      this.dialogFormVisible = false
+      this.$refs.user.resetFields()
     }
   }
 }
 </script>
 
 <style scoped>
-
+.el-row {
+  margin-bottom:20px
+}
 </style>
